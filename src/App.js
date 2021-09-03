6a3ac044b3f93807ad6cd15f527a19eeca2247f9
Tabitha Cooley <tabitha.cooley@ymail.com>
2021-09-03 15:26:27 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import Tabs from './components/Tabs';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tabs, setTabs] = useState([
+    {
+      label:"Tab 1",
+      content:"Tab 1 content is showing here",
+      isSelected: false
+    },
+    {
+      label:"Tab 2",
+      content:"Tab 2 content is showing here.",
+      isSelected: false
+    },
+    {
+      label:"Tab 3",
+      content:"Tab 3 content is showing here.",
+      isSelected: false
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {tabs.map((tab, i) => {
+          return(
+            <Tabs index={i} label={tab.label} tabs={tabs} setTabs={setTabs}/>
+          )
+        })}
+      </div>
+      <div className ="tabStyle">
+        {tabs.map((tab) => {
+          return(
+            tab.isSelected && <div>{ tab.content }</div>
+        )})}
+      </div>
     </div>
   );
 }
